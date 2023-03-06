441434de3ee0d8360a775936240ec4ec2e9c1a53
Maja Sørensen <majabirchsorensen@gmail.com>
2023-03-06 21:30:10 +0100

    more tweets + details

--- main.sql
+++ main.sql
@@ -6,7 +6,7 @@ CREATE TABLE users(
     user_name                   TEXT UNIQUE NOT NULL,
     user_first_name             TEXT NOT NULL,
     user_last_name              TEXT DEFAULT "",
-    user_verified               TEXT NOT NULL,
+    user_verified               INT,
     user_avatar                 TEXT UNIQUE,
     user_cover                  TEXT UNIQUE,
     user_created_at             TEXT NOT NULL,
@@ -17,9 +17,9 @@ CREATE TABLE users(
 ) WITHOUT ROWID;
 
 -- INSERT INTO users VALUES("user_id", "user_name", "user_first_name", "user_last_name", "user_verified", "user_avatar.jpg", "user_cover.jpg", "user_created_at", "user_total_tweets", "user_total_following", "user_total_followers");
-INSERT INTO users VALUES("a9890d6a78a344ec87401cdb85e38a14", "ladygaga", "Lady", "Gaga", "true", "046d38ab9a094b8fb4993d9e2f2131b6.jpg", "b7add1c2b384480bacfdc615c8a3e5aa.jpg", "1204326000", "9936", "116300", "84800000");
-INSERT INTO users VALUES("ebb0d9d74d6c4825b3e1a1bcd73ff49a", "elonmusk", "Elon", "Musk", "true", "0df4ab53a8b14dd0925f6f2d1689c2c6.jpg", "47939906fea84cd28d7c56ac30d63a02.jpg", "1243883319", "23300", "182", "130700000");
-INSERT INTO users VALUES("7860393a03dc4c1e872dcdd2cbf946ab", "RSprachrohr", "Rammstein", "", "true", "0d9b9dd5bde54e338b6335ea0b8eb265.jpg", "c5fbc4cc1aa94940a67c07c873d86352.jpg", "1280689719", "313", "0", "536300");
+INSERT INTO users VALUES("a9890d6a78a344ec87401cdb85e38a14", "ladygaga", "Lady", "Gaga", "1", "046d38ab9a094b8fb4993d9e2f2131b6.jpg", "b7add1c2b384480bacfdc615c8a3e5aa.jpg", "1204326000", "9936", "116300", "84800000");
+INSERT INTO users VALUES("ebb0d9d74d6c4825b3e1a1bcd73ff49a", "elonmusk", "Elon", "Musk", "0", "0df4ab53a8b14dd0925f6f2d1689c2c6.jpg", "47939906fea84cd28d7c56ac30d63a02.jpg", "1243883319", "23300", "182", "130700000");
+INSERT INTO users VALUES("7860393a03dc4c1e872dcdd2cbf946ab", "RSprachrohr", "Rammstein", "", "1", "0d9b9dd5bde54e338b6335ea0b8eb265.jpg", "c5fbc4cc1aa94940a67c07c873d86352.jpg", "1280689719", "313", "0", "536300");
 
 
 CREATE INDEX idx_users_user_first_name ON users(user_first_name);
@@ -53,6 +53,12 @@ INSERT INTO tweets VALUES("7d36ea9c18344874843823487d22c16c", "V MAGAZINE: CHROM
 INSERT INTO tweets VALUES("28106db01fdc496b8f8881e1d943a201", "V MAGAZINE: CHROMATICA BALL DIARIES BY HEDI SLIMANE COVER 2", "67ed34300fa640c38cfc5d01d4d8cca3.jpg", "1663099320", "a9890d6a78a344ec87401cdb85e38a14", "1,733", "9,034", "45,500", "");
 INSERT INTO tweets VALUES("678b621fafbd4492ba9238454dd7c44c", "V MAGAZINE: CHROMATICA BALL DIARIES BY HEDI SLIMANE COVER 3", "a64b696daf5a408289ce85e46f9fc8f2.jpg", "1663423260", "a9890d6a78a344ec87401cdb85e38a14", "622", "3,850", "28,600", "");
 INSERT INTO tweets VALUES("2ad7f04da0c2424588ed67b9b8e723fe", "V MAGAZINE: CHROMATICA BALL DIARIES BY HEDI SLIMANE COVER 4", "4b74287a2f83437d84b7c7a5e10dec7e.jpg", "1663437600", "a9890d6a78a344ec87401cdb85e38a14", "897", "4,178", "31,900", "");
+INSERT INTO tweets VALUES("ea8b05752c1e41a6888b32bb018cca5f", "Folie à Deux 🃏", "46b856c87fa149208960c92430f29336.jpg", "1676888455", "a9890d6a78a344ec87401cdb85e38a14", "6,379", "71,300", "31,900", "16,100,000");
+INSERT INTO tweets VALUES("978f1366cacb44dd8b63205720ccadc5", "CHROMATICA BALL TOUR MERCH AVAILABLE NOW ⚔️💓 http://shop.ladygaga.com", "9befbd1fe2384bef98809599a3519d13.PNG", "1669187160", "a9890d6a78a344ec87401cdb85e38a14", "1,478", "3,438", "16,700", "");
+INSERT INTO tweets VALUES("6ff7ca63bec0476699fe1c33dd2554b7", "I am so honored to be nominated for 2 Grammys for “Hold My Hand” and the Top Gun: Maverick Sountrack with my fellow composers.  It’s a real dream to be included in this celebration of music with a song and musical theme so close to my heart thank you ❤️", "", "1700075160", "a9890d6a78a344ec87401cdb85e38a14", "2,570", "7,112", "50,000", "");
+INSERT INTO tweets VALUES("c15dcee2f0134df497fe0e14b81248da", "I love you little monsters, forever xoxo, Mother Monster #WeLoveYouGaga #ChromaticaBallMiami I’m sorry I used my best judgment, it wasn’t safe.", "", "1695028860", "a9890d6a78a344ec87401cdb85e38a14", "2,700", "5,863", "50,400", "");
+INSERT INTO tweets VALUES("23167c94fd8047f49a6b849b3929fca1", "Joker: Folie à Deux 10.04.24", "dc64f45b25144a479d2013ef2f324a49.PNG", "1691172060", "a9890d6a78a344ec87401cdb85e38a14", "6,586", "81,800", "232,600", "6,800,000");
+INSERT INTO tweets VALUES("2e081c561ce84092aeb7a945fb8d2571", "", "ace24fd7594c4b9da451592cff2a5b0b.jpg", "1690808220", "a9890d6a78a344ec87401cdb85e38a14", "1,443", "4,787", "52,500", "");
 
 CREATE INDEX idx_tweets_tweet_image ON tweets(tweet_image);
 
